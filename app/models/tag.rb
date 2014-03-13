8d81ace89adec6c0733d9ef0773b90dd7ac5537c
Dora <dora2411@gmail.com>
2014-03-13 13:04:16 +0100

    Little change in tag model

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,7 @@
 class Tag < ActiveRecord::Base
   has_many :messages
   
-  validates :type, presence: true , inclusion: {in: %w(basket cart)}
+  validates :category, presence: true , inclusion: {in: %w(basket cart)}
   validates :start_time, presence: true
   
 end
