f32d7f599aedddee841d9691510a5573b2b71054
Ivan Lučev <lucev.ivan@gmail.com>
2014-03-15 14:38:59 +0100

    Readers track tags entering and leaving rows

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -19,23 +19,27 @@ class TagsController < ApplicationController
   end
   
   def enter
+    @reader = Reader.find(params[:reader_id])
     @tag = Tag.find(params[:id])
     if @tag.active == true
       redirect_to action: 'index', notice: 'Tag is already active'
     else
       @tag.active = true
       @tag.save
+      @reader.messages.create(tag_id: @tag.id, activity: 'in')
       redirect_to action: 'index'
     end
   end
   
   def leave
+    @reader = Reader.find(params[:reader_id])
     @tag = Tag.find(params[:id])
     if @tag.active == false
       redirect_to action: 'index', notice: 'Tag is already inactive'
     else
       @tag.active = false
       @tag.save
+      @reader.messages.create(tag_id: @tag.id, activity: 'out')
       redirect_to action: 'index'
     end
   end
--- app/models/reader.rb
+++ app/models/reader.rb
@@ -1,6 +1,11 @@
 class Reader < ActiveRecord::Base
   has_many :messages
+  has_many :tags, through: :messages
   
   validates :name, presence: true, uniqueness: true
   validates :start_time, presence: true
+
+  def tags_waiting
+    self.tags.where(active: true).distinct
+  end
 end
